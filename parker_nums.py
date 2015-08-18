d045380f3eb832c60f0a2fbac142a878ad4e8430
pointofnoreturn <karthik.siva2012@gmail.com>
2015-08-18 06:05:03 -0700

    RK-4 integrator for the Parker (isothermal, stationary) solar wind model.

---
+++ parker_nums.py
@@ -0,0 +1,35 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+# v = u / c
+# x = r / (GM/(2c^2))
+
+def deriv(dY, x, y):
+	derivative = eval(dY)
+	return derivative
+
+def RK4(dY, initX, initY, endX, h):
+	XValues = np.arange(initX, endX + h, h)
+	steps = int(math.ceil(XValues.size))
+	YValues = np.zeros(steps)
+	YValues[0] = initY
+	for i in range(0, steps - 1):
+		k1 = h * deriv(dY, XValues[i], YValues[i])
+		k2 = h * deriv(dY, XValues[i] + 0.5 * h, YValues[i] + 0.5 * k1)
+		k3 = h * deriv(dY, XValues[i] + 0.5 * h, YValues[i] + 0.5 * k2)
+		k4 = h * deriv(dY, XValues[i] + h, YValues[i] + k3)
+		YValues[i + 1] = YValues[i] + k1 / 6 + k2 / 3 + k3 / 3 + k4 / 6
+	return np.array([XValues, YValues])
+
+delta = 0.0001
+dudr = "(2 * y * (x - 1)) / (x * x * (y * y - 1))"
+output1 = RK4(dudr, 1.0 + delta, 1.0 - delta, 2.5, 0.1)
+output2 = RK4(dudr, 1.0 - delta, 1.0 + delta, 0.5, -0.1)
+
+totalOutput = np.concatenate((output1, output2), axis = 1)
+totalOutput = totalOutput[:, totalOutput[0,:].argsort()]
+print totalOutput[0,:].argsort()
+print totalOutput
+plt.plot(totalOutput[0,:], totalOutput[1,:])
+plt.show()
